8e2c33ec3187927ca8ae97c9844a04203598b63b
ThomasMumladze <mumladzea4@gmail.com>
2023-02-15 15:45:33 +0400

    class component uptdade  toggleDarkMode

--- Store/DarkMode.js
+++ Store/DarkMode.js
@@ -3,7 +3,7 @@ export class Theme {
         this.currentTheme = localStorage.getItem('pageTheme')
     }
 
-    toggleDarkMode(body_bg , body_color) {
+    toggleDarkMode(body_bg, body_color) {
         if (this.currentTheme === 'DARK') {
             body_bg.style.background = '#f7f7f8'
             body_color.style.color = "#222"
@@ -15,5 +15,23 @@ export class Theme {
         }
         localStorage.setItem('pageTheme', this.currentTheme)
     }
+
+    toggleDefaultBgChange_onID(id) {
+        if (this.currentTheme === "DARK") {
+            id.style.backgroundColor = "#222"
+        } else {
+            id.style.backgroundColor = '#f7f7f8'
+        }
+    }
+
+    toggleDefaultBgChange(multipleItem) {
+        if (this.currentTheme === "DARK") {
+            multipleItem.forEach(item => item.classList.add('default_background'))
+        } else {
+            multipleItem.forEach(item => item.classList.remove('default_background'))
+        }
+    }
 }
+
+
 
