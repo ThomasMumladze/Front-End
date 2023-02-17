6935f2f3887ccf630ddfb9cc13284b0af6eb956c
ThomasMumladze <mumladzea4@gmail.com>
2023-02-17 10:12:51 +0400

    function call for dark Mode

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Theme } from "./root/darkMode.js"
 const THEME = new Theme()
 
 const header = document.getElementById('header')
+const document_body = document.body
 
 let a_href = document.querySelectorAll('a'),
 LIST_TREE = document.querySelectorAll('.LIST_TREE')
@@ -12,3 +13,10 @@ const darkModeButton = document.querySelector('.darkModeButton').addEventListene
     THEME.Html__linkColor(a_href)
     THEME.html_LIST_wrapperBgChange(LIST_TREE)
 })
+
+// !NoT lose darkTheme On Page Refresh
+import { onRefresh_toggleChanges , onRefresh_bg_changeID_Element , onRefresh_Html__linkColor , onRefresh_html_LIST_wrapperBgChange} from "./root/pageModeOnrRefresh.js"
+onRefresh_toggleChanges(THEME.currentTheme ,  document_body , document_body);
+onRefresh_bg_changeID_Element(THEME.currentTheme , header)
+onRefresh_Html__linkColor(THEME.currentTheme , a_href)
+onRefresh_html_LIST_wrapperBgChange(THEME.currentTheme , LIST_TREE)
