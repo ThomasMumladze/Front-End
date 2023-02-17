e679f4d1586f2b5de73147e1b8a443ec51ba99ed
ThomasMumladze <mumladzea4@gmail.com>
2023-02-17 09:58:16 +0400

    DarkMode NOT Lose On Refresh FIX!

---
+++ root/pageModeOnrRefresh.js
@@ -0,0 +1,33 @@
+export const onRefresh_toggleChanges = (THEME, body_bg, body_color) => {
+    if (THEME === 'dark') {
+        body_bg.style.background = '#343541'
+        body_color.style.color = '#FFF'
+    } else {
+        body_bg.style.background = '#FFF'
+        body_color.style.color = '#222'
+    }
+}
+
+export const onRefresh_bg_changeID_Element = (THEME, ID) => {
+    if (THEME === 'dark') {
+        ID.style.background = '#222'
+    } else {
+        ID.style.background = '#FFF'
+    }
+}
+
+export const onRefresh_Html__linkColor = (THEME, a_href) => {
+    if (THEME === 'dark') {
+        a_href.forEach(item => item.classList.add('a_hrefColor__'))
+    } else {
+        a_href.forEach(item => item.classList.remove('a_hrefColor__'))
+    }
+}
+
+export const onRefresh_html_LIST_wrapperBgChange = (THEME, listWrapper) => {
+    if (THEME === 'dark') {
+        listWrapper.forEach(item => item.classList.add('LIST_WRAPPER'))
+    } else {
+        listWrapper.forEach(item => item.classList.remove('LIST_WRAPPER'))
+    }
+}
